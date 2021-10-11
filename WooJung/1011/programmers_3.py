c5cc37520a3eaec01c22ba778a9fe7c8a36fa765
dnwjddl <woojung980305@gmail.com>
2021-10-11 23:31:54 +0900

    Create programmers_3.py

---
+++ WooJung/1011/programmers_3.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = 0
+    num = [1,2,3,4,5,6,7,8,9]
+    for n in num:
+        if n not in numbers:
+            answer += n
+        else:
+            pass
+    return answer
+  
+  ##########################
+ 
+def solution(numbers):
+    return 45 - sum(numbers)
