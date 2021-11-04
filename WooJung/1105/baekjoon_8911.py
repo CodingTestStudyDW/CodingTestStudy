bb97a48d011c15ef02678694080e54b9e8d9190a
dnwjddl <woojung980305@gmail.com>
2021-11-04 23:24:53 +0900

    Update baekjoon_8911.py

--- WooJung/1105/baekjoon_8911.py
+++ WooJung/1105/baekjoon_8911.py
@@ -1,5 +1,5 @@
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 dirL = [2, 3, 1, 0]
 dirR = [3, 2, 0, 1]
 
@@ -7,8 +7,8 @@ T = int(input())
 for _ in range(T):
     A = input()
     
-    min_r, min_c, max_r, max_c = 0, 0, 0, 0
-    r, c, d = 0, 0, 0		# 현재 위치/방향
+    min_x, min_y, max_x, max_y = 0, 0, 0, 0
+    x, y, d = 0, 0, 0		# 현재 위치/방향
     
     for cmd in A:
         if cmd == "L":		# 방향만 전환
@@ -18,16 +18,16 @@ for _ in range(T):
             d = dirR[d]
             continue
         elif cmd == "F":	# 현재 방향으로 한 칸 이동
-            r += dr[d]
-            c += dc[d]
+            x += dx[d]
+            y += dy[d]
         elif cmd == "B":
-            r -= dr[d]
-            c -= dc[d]
+            x -= dx[d]
+            y -= dy[d]
             
         # 최소 행/열, 최대 행/열 값 갱신
-        min_r = min(min_r, r)
-        min_c = min(min_c, c)
-        max_r = max(max_r, r)
-        max_c = max(max_c, c)
+        min_x = min(min_x, x)
+        min_y = min(min_y, y)
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
 
-    print(abs(max_r - min_r) * abs(max_c - min_c))
+    print(abs(max_y - min_y) * abs(max_x - min_x))
