83b28b5a8e725778e4ebb07a95dfb9abc830b096
dnwjddl <woojung980305@gmail.com>
2021-11-15 12:46:50 +0900

    Update baekjoon_1074.py

--- WooJung/1115/baekjoon_1074.py
+++ WooJung/1115/baekjoon_1074.py
@@ -25,3 +25,31 @@ if r == 1 and c == 0:
     print(cnt + 2)
 if r == 1 and c == 1:
     print(cnt + 3)
+
+    #######################
+    
+ 
+
+
+
+def solve(n,r,c):
+    global result
+    if r==R and c == C:
+        print(int(result))
+        exit(0)
+    # 탐색중인 사각형의 좌표안에 찾는 좌표가 없으면
+    # 사각형의 크기만큼 숫자를 더함
+    if not (r<=R<r+n and c <= C <c+n):
+        result += n*n
+        return
+    
+    solve(n/2, r, c)
+    solve(n/2, r, c+ n/2)
+    solve(n/2, r+ n/2, c)
+    solve(n/2, r+n/2, c+n/2)
+
+    
+    
+result = 0
+N, R, C = map(int(input().split())
+solve(2**N, 0, 0)
